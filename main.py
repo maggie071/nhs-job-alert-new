9c59d9e45cf2e8347eb9ac7997ad871dd7aba693
maggie071 <margaretchai071@gmail.com>
2026-05-25 22:15:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,51 +4,31 @@ import os
 
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 
-URL = "https://findajob.dwp.gov.uk/search?q=assistant+psychologist&loc=86383"
+RSS_URL = "https://findajob.dwp.gov.uk/rss?keywords=assistant+psychologist&location=London"
 
-headers_browser = {
-    "User-Agent": "Mozilla/5.0"
-}
+response = requests.get(RSS_URL)
 
-response = requests.get(URL, headers=headers_browser)
+soup = BeautifulSoup(response.content, "xml")
 
-soup = BeautifulSoup(response.text, "html.parser")
+items = soup.find_all("item")
 
-jobs = soup.find_all("a")
+html_content = "<h1>New Assistant Psychologist Jobs</h1>"
 
-job_list = []
-
-for job in jobs:
-
-    title = job.get_text(strip=True)
-
-    href = job.get("href")
-
-    if href:
-
-        if "assistant psychologist" in title.lower():
-
-            full_link = "https://findajob.dwp.gov.uk" + href
-
-            job_list.append({
-                "title": title,
-                "link": full_link
-            })
-
-html_content = "<h1>New NHS Jobs Found</h1>"
-
-if len(job_list) == 0:
+if len(items) == 0:
 
     html_content += "<p>No jobs found.</p>"
 
 else:
 
-    for job in job_list[:10]:
+    for item in items[:10]:
+
+        title = item.title.text
+        link = item.link.text
 
         html_content += f"""
         <p>
-        <strong>{job['title']}</strong><br>
-        <a href="{job['link']}">{job['link']}</a>
+        <strong>{title}</strong><br>
+        <a href="{link}">{link}</a>
         </p>
         <hr>
         """
